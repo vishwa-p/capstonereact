0a57b0757965774b85917fec0c61e11833a510ab
VISHWA\Vishwa <patelvishwa1701@gmail.com>
2022-10-05 03:02:38 -0400

    error fixed

--- src/components/Login.js
+++ src/components/Login.js
@@ -19,7 +19,7 @@ function Login() {
   
     e.preventDefault();
     try {
-      let res = await fetch("http://127.0.0.1:8000/api/user/authenticate", {
+      let res = await fetch("https://vishwapcapstonelaravel.herokuapp.com/api/user/authenticate", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
