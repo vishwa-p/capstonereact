ee4f52a9c0c561c29dd0e53bec37af22eba293e9
VISHWA\Vishwa <patelvishwa1701@gmail.com>
2022-10-13 11:04:13 -0400

    about page created

--- src/components/About.js
+++ src/components/About.js
@@ -4,12 +4,14 @@ import axios from "axios";
 import ReactPlayer from "react-player";
 
 class Dashboard extends React.Component {
-  
-
   render() {
-    return (
-     <div>Hiii</div>
-    );
+    return <div>
+    <section className="about">
+    <img src="https://i.ibb.co/cc3k0JZ/78c1f289753c602c5a3a5322cd5d7bf0.jpg" className="abtimg"></img>
+    <h2>How we Stared our journey?</h2>
+    <p>My interest in collecting shoes from earlier encouraged me to make something or provide platform for those also who are fond of checking out and collection shoes like me. I thought why don't I make a web application which i can host for users from where they can list of shoes and as a upcoming feature, they will be able to make a cart for their selected shoes and purchase it.</p>
+    </section>
+    </div>;
   }
 }
 export default Dashboard;
